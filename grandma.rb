a825a55f55fec3118e816c7336cfce67e774ce98
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-22 15:44:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string = string.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+  elsif string = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
